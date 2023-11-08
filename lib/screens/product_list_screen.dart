468aaa6d2d491c010fd3d02cf19a5756471f9c53
Ridoy paul <cse.ridoypaul@gmail.com>
2023-11-08 16:06:00 +0600

    updated

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -15,38 +15,57 @@ class _ProductListScreenState extends State<ProductListScreen> {
         title: const Text("Product List"),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            ListTile(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text("Select Action"),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          ListTile(
-                            title: const Text("Edit"),
-                            leading: const Icon(Icons.edit_calendar),
-                            onTap: () {},
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                );
-              },
-              title: Text("This is the title"),
-              leading: Image.network(
-                  'https://topmobileinfo.com/images/products/xiaomi-civi-3-mobile-3858.png'),
-              trailing: Text("\$120"),
-              subtitle: Text("hello"),
-            )
-          ],
+        child: ListView.separated(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return ProductItem();
+          },
+          separatorBuilder: (_, __) => Divider(),
         ),
       ),
+    );
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text("Select Action"),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    title: const Text("Edit"),
+                    leading: const Icon(Icons.edit_calendar),
+                    onTap: () {},
+                  ),
+                  const Divider(
+                    height: 10,
+                  ),
+                  ListTile(
+                    title: const Text("Delete"),
+                    leading: const Icon(Icons.delete_outline),
+                    onTap: () {},
+                  )
+                ],
+              ),
+            );
+          },
+        );
+      },
+      title: Text("This is the title"),
+      leading: Image.network(
+          'https://topmobileinfo.com/images/products/xiaomi-civi-3-mobile-3858.png'),
+      trailing: Text("\$120"),
+      subtitle: Text("hello"),
     );
   }
 }
