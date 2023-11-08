c99736ced8fcbb7876b6099e2d570d3746de9fcd
Ridoy paul <cse.ridoypaul@gmail.com>
2023-11-08 19:42:43 +0600

    updated

--- lib/screens/add_new_product_screen.dart
+++ lib/screens/add_new_product_screen.dart
@@ -8,9 +8,16 @@ class AddNewProductScreen extends StatefulWidget {
 }
 
 class _AddNewProductScreenState extends State<AddNewProductScreen> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController productCodeController = TextEditingController();
+  final TextEditingController productImageLink = TextEditingController();
+  final TextEditingController productQuantityController = TextEditingController();
+  final TextEditingController productUnitPriceController = TextEditingController();
+
+  final GlobalKey<FormState> _addFormKey = GlobalKey<FormState>();
 
   OutlineInputBorder inputBorderStyle = const OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(10)),
+    borderRadius: BorderRadius.all(Radius.circular(5)),
   );
 
   @override
@@ -22,80 +29,110 @@ class _AddNewProductScreenState extends State<AddNewProductScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Product Name",
-                  hintText: "Enter Product Name",
-                  border: inputBorderStyle,
+          child: Form(
+            key: _addFormKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: titleController,
+                  decoration: InputDecoration(
+                    labelText: "Product Name",
+                    hintText: "Enter Product Name",
+                    border: inputBorderStyle,
+                  ),
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return "Please Enter any Value";
+                    }
+                    return null;
+                  },
                 ),
-                validator: (String? value) {
-                  if (value!.isEmpty) {
-                    return "Please Enter any Task";
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10,),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Product Code",
-                  hintText: "Enter Product Code",
-                  border: inputBorderStyle,
+                const SizedBox(
+                  height: 10,
                 ),
-                validator: (String? value) {
-                  if (value!.isEmpty) {
-                    return "Please Enter any Task";
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10,),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Product Image Link",
-                  hintText: "Enter Product Image Link",
-                  border: inputBorderStyle,
+                TextFormField(
+                  controller: productCodeController,
+                  decoration: InputDecoration(
+                    labelText: "Product Code",
+                    hintText: "Enter Product Code",
+                    border: inputBorderStyle,
+                  ),
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return "Please Enter any Value";
+                    }
+                    return null;
+                  },
                 ),
-                validator: (String? value) {
-                  if (value!.isEmpty) {
-                    return "Please Enter any Task";
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10,),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: "Product Quantity",
-                  hintText: "Enter Product Quantity",
-                  border: inputBorderStyle,
+                const SizedBox(
+                  height: 10,
                 ),
-                validator: (String? value) {
-                  if (value!.isEmpty) {
-                    return "Please Enter any Task";
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10,),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: "Product Unit Price",
-                  hintText: "Enter Product Unit Price",
-                  border: inputBorderStyle,
+                TextFormField(
+                  controller: productImageLink,
+                  decoration: InputDecoration(
+                    labelText: "Product Image Link",
+                    hintText: "Enter Product Image Link",
+                    border: inputBorderStyle,
+                  ),
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return "Please Enter any Value";
+                    }
+                    return null;
+                  },
                 ),
-                validator: (String? value) {
-                  if (value!.isEmpty) {
-                    return "Please Enter any Task";
-                  }
-                  return null;
-                },
-              ),
-            ],
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: productQuantityController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Product Quantity",
+                    hintText: "Enter Product Quantity",
+                    border: inputBorderStyle,
+                  ),
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return "Please Enter any Value";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: productUnitPriceController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Product Unit Price",
+                    hintText: "Enter Product Unit Price",
+                    border: inputBorderStyle,
+                  ),
+                  validator: (String? value) {
+                    if (value!.isEmpty) {
+                      return "Please Enter any Value";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if(_addFormKey.currentState!.validate()) {
+
+                      }
+                    },
+                    child: const Text("Add Confrim"),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
